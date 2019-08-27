1b743de31cca3ced600d5069e2fa00cec183980d
celine lemenn <celinelemenn@gmail.com>
2019-08-27 16:27:20 +0200

    FIX removed the unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'bookmarks/create'
-  get 'reports/new'
-  get 'reports/create'
-  get 'downvotes/create'
-  get 'likes/create'
-  get 'comments/new'
-  get 'comments/create'
   devise_for :users
   root to: 'pages#home', as: :homepage
 
