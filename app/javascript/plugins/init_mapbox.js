47a5a5f5e73fcb9fdf73e3ef015df0cbb762a881
Margarita <l.m.klingen@gmail.com>
2019-08-27 15:18:30 +0200

    FEATURE_created_iniital_seeding_POIs- with only lat long

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -5,12 +5,25 @@ const initMapbox = () => {
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+
+     // initialise map
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/klingmap/cjztq9yns04ke1dny5m5o49bz',
       center: [4.925,52.375],
       zoom: 10,
     });
+
+    // drawing markers on the map
+    const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach((marker) => {
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .addTo(map);
+    });
+
+
+
   }
 };
 
