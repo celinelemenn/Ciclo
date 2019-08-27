ffb1da78f284b2ebc2335966bf9bfabda1ea03bb
Arjun Puri <arjun_261094@hotmail.com>
2019-08-27 17:54:29 +0200

    finished making the highlighter and changed navbar from fixed to sticky

--- app/javascript/plugins/navbar_highlighter.js
+++ app/javascript/plugins/navbar_highlighter.js
@@ -1,15 +1,30 @@
 const highlighter = () => {
   const path = document.location.pathname
   const linksPath = document.querySelectorAll('.anchor_link')
+  const items = document.querySelectorAll('.navbar-item')
+  // const links = []
+  // items.forEach((item) => {
+  //   const obj = new Object;
+  //   obj.path = item.firstElementChild.pathname;
+  //   obj.html = item;
+  //   links.push(obj);
+  // })
+  // console.log(links);
+  // links.forEach((link) => {
+  //   link.html.classList.remove('active')
+  //   if (document.location.pathname === link.path) {
+  //     link.html.classList.add('active')
+  //   }
+  // })
 
   linksPath.forEach((link) => {
-    const items = document.querySelector('.navbar-item')
     items.forEach((item) => {
+      item.classList.remove('active')
       if (path === link.pathname) {
         item.classList.add('active');
       }
     });
   });
+}
 
-
-  export { highlighter };
+export { highlighter };
