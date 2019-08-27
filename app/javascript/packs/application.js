adeb7c3f4a68cc39fd9fc285f590602f6b54cb8c
Arjun Puri <arjun_261094@hotmail.com>
2019-08-27 19:02:30 +0200

    FEATURE_ added MY JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@ import "bootstrap";
 
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
-// import { highlighter } from '../plugins/navbar_highlighter';
+import { highlighter } from '../plugins/navbar_highlighter';
 
 initMapbox();
-// highlighter();
+highlighter();
--- app/javascript/plugins/navbar_highlighter.js
+++ app/javascript/plugins/navbar_highlighter.js
@@ -1,30 +1,14 @@
 const highlighter = () => {
   const path = document.location.pathname
   const linksPath = document.querySelectorAll('.anchor_link')
-  const items = document.querySelectorAll('.navbar-item')
-  // const links = []
-  // items.forEach((item) => {
-  //   const obj = new Object;
-  //   obj.path = item.firstElementChild.pathname;
-  //   obj.html = item;
-  //   links.push(obj);
-  // })
-  // console.log(links);
-  // links.forEach((link) => {
-  //   link.html.classList.remove('active')
-  //   if (document.location.pathname === link.path) {
-  //     link.html.classList.add('active')
-  //   }
-  // })
+  const activeItems = document.querySelectorAll('.navbar-item.active')
+
+  activeItems.forEach(item => item.classList.remove('active'))
 
   linksPath.forEach((link) => {
-    link.parentElement.classList.toggle('active')
-    items.forEach((item) => {
-      // item.classList.remove('active')
-      if (path === link.pathname) {
-        item.classList.add('active');
-      }
-    });
+    if (path === link.pathname) {
+      link.parentElement.classList.add('active');
+    }
   });
 }
 
