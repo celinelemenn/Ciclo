f08a2000829586d102485464e6a66384d7a5fb4a
celine lemenn <celinelemenn@gmail.com>
2019-08-28 15:41:01 +0200

    Seeds updated for like/ downvote

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,15 @@
 class LikesController < ApplicationController
   def create
+    @like = Like.new
+    @like.user = User.find(params[:user])
+    @like.point_of_interest = PointOfInterest.find(params[:point_of_interest_id])
+
+    @like.save
+  end
+
+private
+
+  def like_params
+    # params.requires(:like).permit(:)
   end
 end
