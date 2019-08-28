24e07340ff0b0b7dcf1a4879fbce5e583788bf42
Arjun Puri <arjun_261094@hotmail.com>
2019-08-28 15:34:39 +0200

    ADDED_adds terms and conditions to user table and to the signup form. They are linked, terms default is true, therfore it is green in the form as it is validated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_28_092654) do
+ActiveRecord::Schema.define(version: 2019_08_28_131630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -134,6 +134,7 @@ ActiveRecord::Schema.define(version: 2019_08_28_092654) do
     t.string "photo"
     t.boolean "share_location"
     t.string "blog"
+    t.boolean "terms", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
