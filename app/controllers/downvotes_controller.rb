85de2e351d8134e6f5f495c0628f3f70e489580e
celine lemenn <celinelemenn@gmail.com>
2019-08-28 16:13:46 +0200

    downvote works the same as like

--- app/controllers/downvotes_controller.rb
+++ app/controllers/downvotes_controller.rb
@@ -1,4 +1,21 @@
 class DownvotesController < ApplicationController
   def create
+    @downvote = Downvote.new
+
+    @user = User.find(params[:user])
+    @downvote.user = @user
+
+    @poi = PointOfInterest.find(params[:point_of_interest_id])
+    @downvote.point_of_interest = @poi
+
+    @existing_downvote = Downvote.find_by user: @user, point_of_interest: @poi
+
+    if @existing_downvote.nil?
+      @downvote.save
+      render 'point_of_interests/show', point_of_interest: @poi
+    else
+      @existing_downvote.delete
+      render 'point_of_interests/show', point_of_interest: @poi
+    end
   end
 end
