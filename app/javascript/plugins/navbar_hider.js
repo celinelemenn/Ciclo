82da19bd0db2f09ce69f9079b63eb55a1e451ad0
Arjun Puri <arjun_261094@hotmail.com>
2019-08-29 13:24:06 +0200

    ADDED_finishes creating the JS hider and fixing the forms display on mobile

--- app/javascript/plugins/navbar_hider.js
+++ app/javascript/plugins/navbar_hider.js
@@ -1,3 +1,10 @@
 const hider = () => {
-  const path = document.location.pathname
+  const element = document.querySelector('.uni-app-container');
+  const pageUrl = window.location.href;
+  if (pageUrl.indexOf("users") > -1) {
+    element.classList.toggle("uni-app-container");
+    element.classList.toggle("uni-app-container-no-navbar")
+  };
 };
+
+export { hider };
