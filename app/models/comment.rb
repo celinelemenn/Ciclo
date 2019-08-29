96dfb8e6d2c2b1c81b8a25b5e30bffadbdb4c8c0
celine lemenn <celinelemenn@gmail.com>
2019-08-29 17:12:35 +0200

    added dates to the comment in the comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,19 @@
 class Comment < ApplicationRecord
   belongs_to :point_of_interest
   belongs_to :user
+
+  def date
+    date = -(self.created_at - DateTime.now) / 60
+
+    if date < 60
+      date = "#{date} min ago"
+    elsif date > 60 || date < 1440
+      date = "#{(date/60).round} hours ago"
+    else
+      date = (date / 1440).round
+      date = "#{date} days ago"
+    end
+    date
+  end
+
 end
