05c7e2b94b663f8fb440b851969cba99022ac1d7
Christina Meliniotou <chris.melinioti@gmail.com>
2019-08-30 13:51:08 +0200

    Action create and display on view

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,18 +1,19 @@
 class BookmarksController < ApplicationController
   def create
     @bookmark = Bookmark.new
-    @user = User.find(params[:id])
+    @comment = Comment.new
+    @user = User.find(params[:user])
 
-    @poi.PointOfInterest.find(params[:point_of_interest_id])
+    @poi = PointOfInterest.find(params[:point_of_interest_id])
     @bookmark.point_of_interest = @poi
 
     @existing_bookmark = Bookmark.find_by user: @user, point_of_interest: @poi
 
     if @existing_bookmark.nil?
       @bookmark.save
-      render 'bookmark/show', point_of_interest: @poi
+      render 'point_of_interests/show', point_of_interest: @poi
     else
-      @existing_downvote.delete
+      @existing_bookmark.delete
       render 'point_of_interests/show', point_of_interest: @poi
   end
  end
