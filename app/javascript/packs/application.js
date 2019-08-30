fa985fd0ecff268602736f6bbc961364deb78d5b
Christina Meliniotou <chris.melinioti@gmail.com>
2019-08-30 15:45:33 +0200

    Javascript toggle on bookmark icon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,8 +3,10 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { highlighter } from '../plugins/navbar_highlighter';
 import { run_mapbox_page } from '../plugins/mapbox/init_mapbox';
-import {hider} from '../plugins/navbar_hider';
+import { hider } from '../plugins/navbar_hider';
+import { bookmark_highlighter } from '../plugins/bookmark_highlighter';
 
 run_mapbox_page();
 highlighter();
 hider();
+bookmark_highlighter();
