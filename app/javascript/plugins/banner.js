f4cdc5e4fa766fc726b48ebfeb61510592c67cca
Arjun Puri <arjun_261094@hotmail.com>
2019-09-01 23:59:19 +0200

    STYLING- created a new style for the homepage

--- app/javascript/plugins/banner.js
+++ app/javascript/plugins/banner.js
@@ -4,8 +4,8 @@ const loadDynamicBannerTitle = () => {
   new Typed('#banner-typed-text', {
     strings: ["Get real-time reports from fellow cyclists",
               "Discover peer-reviewed hot-spots",
-              "Take the road less travelled, with confidence"],
-    typeSpeed: 55,
+              "Take the road less travelled, with confidence."],
+    typeSpeed: 50,
     loop: false
   });
 }
