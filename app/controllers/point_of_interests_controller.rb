05cc5cd77ebf46819a9378fe46c4c0564e9d6271
celine lemenn <celinelemenn@gmail.com>
2019-09-02 13:38:35 +0200

    FEATURE edit POI working and styled

--- app/controllers/point_of_interests_controller.rb
+++ app/controllers/point_of_interests_controller.rb
@@ -1,5 +1,5 @@
 class PointOfInterestsController < ApplicationController
-  before_action :find_poi, only: [:show]
+  before_action :find_poi, only: [:show, :edit, :update]
 
   def index
     @user_poi = PointOfInterest.select { |poi| poi.user == current_user}
@@ -19,12 +19,24 @@ class PointOfInterestsController < ApplicationController
   end
 
   def update
+    @poi.title = params[:point_of_interest]['title']
+    @poi.poi_type = params[:point_of_interest]['poi_type']
+    @poi.description = params[:point_of_interest]['description']
+    @poi.photo = params[:point_of_interest]['photo']
+    @poi.published = true
+
+    if @poi.save
+      redirect_to point_of_interest_path(@poi)
+
+    else
+      render :edit
+    end
   end
 
   private
 
   def poi_params
-
+    params.require(:point_of_interest).permit(:poi_type, :title, :description, :photo)
   end
 
   def find_poi
