ad9c4b9998102b61d8f4a12ede06300aac90a03d
Arjun Puri <arjun_261094@hotmail.com>
2019-09-02 15:52:10 +0200

    ADDED_creates API controller for user positions to post user positions to the database

--- app/javascript/plugins/geolocalizer.js
+++ app/javascript/plugins/geolocalizer.js
@@ -3,49 +3,49 @@ import mapboxgl from 'mapbox-gl';
 import { map } from './mapbox/init_mapbox.js';
 import currentMarker from '../../assets/images/current_marker.png'
 
-  const locateButton = document.querySelector(".btn-on-map-right");
-  const options = { enableHighAccuracy: true };
-  const userCurrentPosition = [];
+const locateButton = document.querySelector(".btn-on-map-right");
+const options = { enableHighAccuracy: true };
+const userCurrentPosition = [];
 
-  // The currentPosition function is called as the first argument of
-  // the getCurrentPosition function and returns a Position object
-  // which contains a coordinates object.
+// The currentPosition function is called as the first argument of
+// the getCurrentPosition function and returns a Position object
+// which contains a coordinates object.
 
-  const currentPosition = (position) => {
-    const userCoordinates = position.coords
-    // console.log(`Current Latitude : ${userCoordinates.latitude}`);
-    // console.log(`Current Longitude: ${userCoordinates.longitude}`);
-    // console.log(`Current Accuracy: ${userCoordinates.accuracy} meters.`);
-    // console.log(`Starting Position: Lat: ${userCoordinates.latitude} Long: ${userCoordinates.longitude}`);
+const currentPosition = (position) => {
+  const userCoordinates = position.coords
+  // console.log(`Current Latitude : ${userCoordinates.latitude}`);
+  // console.log(`Current Longitude: ${userCoordinates.longitude}`);
+  // console.log(`Current Accuracy: ${userCoordinates.accuracy} meters.`);
+  // console.log(`Starting Position: Lat: ${userCoordinates.latitude} Long: ${userCoordinates.longitude}`);
+}
+
+// The function below is optional and holds an error message. It is an option
+// argument of the getCurrentPosition function.
+
+const error = (error) => {
+  console.warn(`ERROR(${error.code}): ${error.message}`);
+}
+
+// Below is the function to watch a user's position
+
+const watchUserPosition = (position) => {
+  const watchedCoordinates = position.coords
+  const currentPosition = {
+    latitude: watchedCoordinates.latitude,
+    longitude: watchedCoordinates.longitude
   }
+  console.log('Array: ', userCurrentPosition)
+  userCurrentPosition.push(currentPosition);
 
-  // The function below is optional and holds an error message. It is an option
-  // argument of the getCurrentPosition function.
+  // console.log(`Watched Coordinates Latitude : ${watchedCoordinates.latitude}`);
+  // console.log(`Watched Coordinates Longitude: ${watchedCoordinates.longitude}`);
+  // console.log(`Watched Coordinates Accuracy: ${watchedCoordinates.accuracy} meters.`);
+  console.log(`Watched Position: Lat: ${watchedCoordinates.latitude} Long: ${watchedCoordinates.longitude}`);
+}
 
-  const error = (error) => {
-    console.warn(`ERROR(${error.code}): ${error.message}`);
-  }
-
-  // Below is the function to watch a user's position
-
-  const watchUserPosition = (position) => {
-    const watchedCoordinates = position.coords
-    const currentPosition = {
-      latitude: watchedCoordinates.latitude,
-      longitude: watchedCoordinates.longitude
-    }
-    console.log('Array: ', userCurrentPosition)
-    userCurrentPosition.push(currentPosition);
-
-    // console.log(`Watched Coordinates Latitude : ${watchedCoordinates.latitude}`);
-    // console.log(`Watched Coordinates Longitude: ${watchedCoordinates.longitude}`);
-    // console.log(`Watched Coordinates Accuracy: ${watchedCoordinates.accuracy} meters.`);
-    console.log(`Watched Position: Lat: ${watchedCoordinates.latitude} Long: ${watchedCoordinates.longitude}`);
-  }
-
-  // The condition below only runs the getCurrentPosition function
-  // if the locator button is present AND the browser is capable of using
-  // geolocation.
+// The condition below only runs the getCurrentPosition function
+// if the locator button is present AND the browser is capable of using
+// geolocation.
 const geolocator = () => {
   window.addEventListener("load", (event) => {
     console.log("This works");
@@ -91,6 +91,5 @@ const geolocator = () => {
     });
   }
 }
-
 
 export { geolocator };
