b0fa32f9453e7d416abe06c0382c1910faf65282
Margarita <l.m.klingen@gmail.com>
2019-09-02 11:06:33 +0200

    REFACOTERING MAPBOX related functions

---
+++ app/javascript/plugins/mapbox/add_pois_to_map.js
@@ -0,0 +1,27 @@
+import { createPopdown  } from '../../plugins/mapbox/create_popdown';
+import mapboxgl from 'mapbox-gl';
+
+const addPoisToMap = (map, markers) => {
+
+  markers.forEach((marker) => {
+    const el = document.createElement('div');
+    el.className = 'marker poi-icon';
+    const url = marker.marker_link;
+    el.style.backgroundImage = `url(${url})`;
+    el.style.width = '30px';
+    el.style.height = '30px';
+
+    // drop down
+    el.addEventListener('click', function() {
+      createPopdown(marker.infoWindow);
+    });
+
+    new mapboxgl.Marker(el)
+      .setLngLat([ marker.lng, marker.lat ])
+      .addTo(map);
+
+  });
+
+};
+
+export { addPoisToMap }
