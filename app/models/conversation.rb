0059771fec578bfacce44ee73670e5df3cb060b1
Arjun Puri <arjun_261094@hotmail.com>
2019-09-03 14:08:21 +0200

    ADDED_adds has many association for personal messages and sets default sorting criteria for personal messages in a convo

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -6,8 +6,14 @@ class Conversation < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :receiver, class_name: 'User'
 
+  # The association below is added to specify the has_many relationship with personalMessages
+  # It also sets the default sorting so that the latest message always displays first. Lastly,
+  # if you destroy a conversation, you destory all of its messages too.
+
+  has_many :personal_messages, -> { order(created_at: :asc) }, dependent: :destroy
+
   # The validation below ensures that there cannot be more than on converation
   # between a pair of users
 
-  validates :author, uniqueness: { scope: :reciever}
+  validates :author, uniqueness: { scope: :reciever }
 end
