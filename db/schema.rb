0250d380786dddc659dd01efee7d2ba0a1a16052
Arjun Puri <arjun_261094@hotmail.com>
2019-09-03 12:12:47 +0200

    DELETE_drops the conversation and messages table and deletes the corresponding models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_30_103901) do
+ActiveRecord::Schema.define(version: 2019_09_03_100529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,11 +34,6 @@ ActiveRecord::Schema.define(version: 2019_08_30_103901) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "conversations", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "downvotes", force: :cascade do |t|
     t.bigint "point_of_interest_id"
     t.bigint "user_id"
@@ -55,11 +50,6 @@ ActiveRecord::Schema.define(version: 2019_08_30_103901) do
     t.datetime "updated_at", null: false
     t.index ["point_of_interest_id"], name: "index_likes_on_point_of_interest_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "point_of_interests", force: :cascade do |t|
