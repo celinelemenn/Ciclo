31a04487382142e705750733cf7f25a41f7a19bc
Arjun Puri <arjun_261094@hotmail.com>
2019-09-03 14:09:44 +0200

    ADDED_adds validation to personal message that it must have a body

--- app/models/personal_message.rb
+++ app/models/personal_message.rb
@@ -1,4 +1,8 @@
 class PersonalMessage < ApplicationRecord
   belongs_to :conversation
   belongs_to :user
+
+  # Obviously, we don't want to send messages that are empty so:
+
+  validates :body, presence: true
 end
