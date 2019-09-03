03e994db0985e49053ddc67c2313cad64ea5316f
Arjun Puri <arjun_261094@hotmail.com>
2019-09-03 14:30:12 +0200

    ADDED_ adds a before action method to authenticate the user to the conversations controller. Also defines an index action to display all conversations

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,5 +1,10 @@
 class ConversationsController < ApplicationController
+  # We want to make sure that the user is logged in before they are able to create a conversation
+
+  before_action :authenticate_user!
+
   def index
+    @conversations = Conversation.participating(current_user).order('updated_at DESC')
   end
 
   def create
