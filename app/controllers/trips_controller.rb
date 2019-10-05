81a6714604017732b5ffe47a4724c5167120b0d9
celine lemenn <celinelemenn@gmail.com>
2019-10-05 18:22:20 +0200

    Added the cycling route dropdown

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,6 +1,12 @@
 class TripsController < ApplicationController
   def new
     @trip = Trip.new
+    @cyling_routes = [
+      'EuroVelo1', 'EuroVelo2', 'EuroVelo3', 'EuroVelo4', 'EuroVelo5', 'EuroVelo6',
+      'EuroVelo7', 'EuroVelo8', 'EuroVelo9', 'EuroVelo10', 'EuroVelo11', 'EuroVelo12',
+      'EuroVelo15', 'EuroVelo17', 'EuroVelo19',
+      'Silk Road', 'South East Asia', 'South America West Coast', 'US Pacific Coast', 'US East to West'
+    ]
   end
 
   def create
