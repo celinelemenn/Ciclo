617fe723e0233addfd691be47ce845d44d7c33b3
celine lemenn <celinelemenn@gmail.com>
2019-10-06 13:35:35 +0200

    Trip : end date can be blank + km can be blank

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -8,6 +8,7 @@ class Trip < ApplicationRecord
 
   def deadline_is_possible?
     # return if end_date.blank? || start_date.blank?
+    end_date = Date.today if end_date.nil?
 
     errors.add(:start_date, "must be earlier than end date") if end_date < start_date
   end
@@ -16,4 +17,10 @@ class Trip < ApplicationRecord
       'EuroVelo1 - Atlantic Coast Route', 'EuroVelo2 - Capitals Route', 'EuroVelo3 - Pilgrims Route', 'EuroVelo4 - Central Europe Route', 'EuroVelo5 - Via Romea (Francigena)', 'EuroVelo6 - Atlantic – Black Sea','EuroVelo7 - Sun Route', 'EuroVelo8 - Mediterranean Route', 'EuroVelo9 - Baltic – Adriatic', 'EuroVelo10 - Baltic Sea Cycle Route', 'EuroVelo11 - Baltic Sea Cycle Route', 'EuroVelo12 - North Sea Cycle Route', 'EuroVelo15 - Rhine Cycle Route', 'EuroVelo17 - Rhone Cycle Route', 'EuroVelo19 - Meuse Cycle Route',
       'Silk Road', 'South East Asia', 'South America - West Coast', 'US - Pacific Coast', 'US - East to West', 'Africa - West Coast', 'Africa - East Coast'
     ]
+
+  def trip_time(start_date, end_date)
+    self.end_date = Date.today if end_date.nil?
+
+    (self.start_date.to_date..self.end_date.to_date).to_a.size
+  end
 end
