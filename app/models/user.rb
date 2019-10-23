2935331ba505ac8d3bd481ead47c0e5abbc983cf
celine lemenn <celinelemenn@gmail.com>
2019-10-23 16:16:40 +0200

    now facebook button works but not styled

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  devise :omniauthable, omniauth_providers: %i[facebook]
+
 
   has_many :point_of_interests
   has_many :preferences
@@ -45,5 +47,21 @@ class User < ApplicationRecord
       ""
     end
   end
+
+
+  def self.from_omniauth(auth)
+  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    user.email = auth.info.email
+    user.password = Devise.friendly_token[0, 20]
+    user.full_name = auth.info.name  # assuming the user model has a name
+    # raise
+    # user.remote_photo_url = auth.info.image
+    user.remote_photo_url = "https://res.cloudinary.com/dwkwy2q1n/image/facebook/#{user.uid}.jpg"
+    # user.save! # assuming the user model has an image
+    # If you are using confirmable and the provider(s) you use validate emails,
+    # uncomment the line below to skip the confirmation emails.
+    # user.skip_confirmation!
+  end
+end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get 'feedback/create'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users, :controllers  => {:registrations => 'registrations'}
+  devise_for :users, controllers: {:registrations => 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
 
   require "sidekiq/web"
   authenticate :user, lambda { |u| u.admin } do
