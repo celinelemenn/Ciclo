df38cc78f33723453c8cac16a25594a80fcd6d20
celine lemenn <celinelemenn@gmail.com>
2020-01-01 18:30:49 +0100

    migrated trackable and added redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,13 +36,13 @@ class ApplicationController < ActionController::Base
       welcomehome_path
     else map_path
     end
-    # sign_up_url = user_registration_url
-    # if url_for == sign_up_url
-    # # if resource === new_user_registration_path
-    #   map_path
-    # else
-    #   map_path
-    # end
-    # return the path based on resource
+  #   # sign_up_url = user_registration_url
+  #   # if url_for == sign_up_url
+  #   # # if resource === new_user_registration_path
+  #   #   map_path
+  #   # else
+  #   #   map_path
+  #   # end
+  #   # return the path based on resource
   end
 end
