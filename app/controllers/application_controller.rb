9a60227dbe787127ab76b96f9b75d9120d7af37c
celine lemenn <celinelemenn@gmail.com>
2020-01-01 18:14:59 +0100

    added redirect and trackable in user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,14 +31,18 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:full_name, :bio, :blog, :description, :photo, :share_location, :terms, :status])
  end
 
-  # def after_sign_in_path_for(resource)
-  #   sign_up_url = user_registration_url
-  #   if url_for == sign_up_url
-  #   # if resource === new_user_registration_path
-  #     map_path
-  #   else
-  #     map_path
-  #   end
-  #   # return the path based on resource
-  # end
+  def after_sign_in_path_for(resource)
+    if resource.sign_in_count == 1
+      welcomehome_path
+    else map_path
+    end
+    # sign_up_url = user_registration_url
+    # if url_for == sign_up_url
+    # # if resource === new_user_registration_path
+    #   map_path
+    # else
+    #   map_path
+    # end
+    # return the path based on resource
+  end
 end
