6064c28e67094a5d819841be86fdff0b4dec2d10
celine lemenn <celinelemenn@gmail.com>
2020-02-16 11:54:18 +0100

    trying to solve the precompiling assets failing

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module Ciclo
   class Application < Rails::Application
     config.action_view.embed_authenticity_token_in_remote_forms = true
@@ -18,6 +19,7 @@ module Ciclo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.autoload_paths << Rails.root.join('lib')
+    config.assets.initialize_on_precompile = false
     # config.force_ssl = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
