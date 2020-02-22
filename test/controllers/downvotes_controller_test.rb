3eba8003ef0307f243c2716569d781d5eb1ade1d
celine lemenn <celinelemenn@gmail.com>
2020-02-22 15:25:01 +0100

    added rspec gem

--- test/controllers/downvotes_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class DownvotesControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get downvotes_create_url
-    assert_response :success
-  end
-
-end
