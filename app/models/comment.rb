998494e7036056e37269e3b18642e2f1b9f61359
celine lemenn <celinelemenn@gmail.com>
2020-05-22 22:38:31 +0200

    fix big with comment date that was bugging poi page

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,10 +7,17 @@ class Comment < ApplicationRecord
   def date
     date = -(created_at - DateTime.now) / 60
 
-    date = "#{date.round} min ago" if date < 60
-    date = "#{(date / 60).round} hours ago" if date > 60 && date < 1440
-    date = "#{(date / 1440).round} days ago" if date > 1440 && date < 43_200
-    date = "#{(date / 43_200).round} months ago" if date > 43_200
-    return date
+    if date < 60
+      date = "#{date.round} min ago"
+    elsif date > 60 && date < 1440
+      date = "#{(date / 60).round} hours ago"
+    elsif date > 1440 && date < 43_200
+      date = (date / 1440).round
+      date = "#{date} days ago"
+    else
+      date = (date / 43_200).round
+      date = "#{date} months ago"
+    end
+    date
   end
 end
